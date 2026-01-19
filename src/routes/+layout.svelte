b85f8c8f875c2b37e74428170d4590ae3dae4706
Heinrichkarl72 <121289455+Heinrichkarl72@users.noreply.github.com>
2026-01-19 10:03:21 +0100

    Video 5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,16 @@
 <script>
 	import '../app.css';
+	import { store } from '$lib/store.svelte.js';
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 	let theme = $state('halloween');
+
+	// unser "Konstruktor" (lifecycle hook) - läuft jedesmal, wenn die Seite bzw. die Komponente geladen wird:
+	$effect(() => {
+		store.listRabbits();
+		$inspect('🐰: ', store.rabbits);
+	});
 </script>
 
 <svelte:head>
@@ -11,8 +18,8 @@
 </svelte:head>
 
 <div data-theme={theme} class=" min-h-screen p-2">
-	<nav class="flex items-center justify-between">
-		<a href="/" class="text-4xl">🐰</a>
+	<nav class=" flex items-center justify-between">
+		<a href="/" class=" text-4xl">🐰</a>
 		<select class="select w-[150px]" bind:value={theme}>
 			<option disabled selected>Pick a theme</option>
 			<option value="light">light</option>
@@ -22,7 +29,7 @@
 			<option value="halloween">halloween</option>
 		</select>
 	</nav>
-	<main class="flex-col items-center justify-center">
+	<main class="itmes-center flex-col justify-center">
 		{@render children?.()}
 	</main>
 </div>
